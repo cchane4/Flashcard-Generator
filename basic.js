f4aa3e5160a57e3845adfe272847c151936a7492
Chad Chaney <cchane4@gmail.com>
2017-12-28 14:24:25 -0600

    added a promise to create new flashcard

--- basic.js
+++ basic.js
@@ -9,9 +9,23 @@ function BasicFlashcard = function(frontSide,backSide) {
 }
 
 
-//inquirer promise collects the front-side and back-side information 
+//inquirer promise collects the front-side and back-side information and
+//creates and instance of a new basic flashcard then console.logs the card  
+inquirer.prompt([{
+	type: "input", 
+	name: "frontside", 
+	message: "What is the question for you would like to ask?"
+},{ 
+	type: "input", 
+	name: "backside",
+	message: "What is the answer to the question?"
+}]).then(function(inputs){ 
+	var card = new BasicFlashcard(inputs.frontSide, inputs.backSide)
+	console.log(card); 
+})
 
-BasicFlashcard.prototype.save = function(object){
+
+/*BasicFlashcard.prototype.save = function(object){
 	fs.appendFile("basic_card.txt", object, function(error){
 		if (error) throw error;
 		console.log("Successfully added to the database");
@@ -19,4 +33,4 @@ BasicFlashcard.prototype.save = function(object){
 }
 
 
-module.exports = BasicFlashcard;
+module.exports = BasicFlashcard;*/ 
