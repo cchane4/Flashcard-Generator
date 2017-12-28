9336c2747b137e39802aab0284d09c0d9219fc46
Chad Chaney <cchane4@gmail.com>
2017-12-28 14:16:57 -0600

    added a new BasicFlashcard constructor

--- basic.js
+++ basic.js
@@ -1,17 +1,20 @@
-var fs = require("fs"); 
+var fs = require("fs");
+var inquirer = require(inquirer);
 
 
+// takes in a pair of strings from the front-side and the back-side 
+function BasicFlashcard = function(frontSide,backSide) {
+	this.frontSide;
+	this.backSide;
+}
 
- 
-BasicFlashcard = function() {
-	this.front,
-	this.back
-} 
-	
+
+//inquirer promise collects the front-side and back-side information 
+
 BasicFlashcard.prototype.save = function(object){
-	fs.appendFile("basic_card.txt", object, function(error){ 
+	fs.appendFile("basic_card.txt", object, function(error){
 		if (error) throw error;
-		console.log("Successfully added to the database"); 
+		console.log("Successfully added to the database");
 	});
 }
 
